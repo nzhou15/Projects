08bd68e6a5aa1b1ca283b0719000774d74be5c61
nzhou15 <94396910+nzhou15@users.noreply.github.com>
2022-05-06 14:05:42 -0400

    Delete project directory

--- project/addToWishlist.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $myfile = fopen("./wishlist.csv","ab") or die("Unable to open file!");
-    //profid, profname, term_month_year, course_type, course_num, TA_name, student_ID
-    $data = $_POST["wishlist"];
-    fwrite($myfile, $data . "\r\n");
-    fclose($myfile);
-    $message = "Add successfully!";
-    echo "<script type='text/javascript'>alert('$message'); history.go(-1); </script>";
-?>
